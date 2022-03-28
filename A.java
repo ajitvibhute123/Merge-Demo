02946d68c1e8e30a4c8514b6efe0ae791477f6c0
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-03-28 22:32:17 +0530

    My First Commit

---
+++ A.java
@@ -0,0 +1,20 @@
+
+class SingleInheritance {
+
+	SingleInheritance() {
+		System.out.println("  this is Ajit");
+	}
+
+}
+
+public class A {
+	A() {
+		System.out.println("Myself is Ajit");
+	}
+
+	public static void main(String[] args) {
+
+		SingleInheritance aa = new SingleInheritance();
+
+	}
+}
